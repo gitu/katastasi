88970ea8987172c55370e93b1350bdcd19438195
Florian Schrag <f@schr.ag>
2023-09-17 19:59:26 +0200

    reimport

--- pkg/core/config_from_kubeconfig.go
+++ pkg/core/config_from_kubeconfig.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/gitu/katastasi/pkg/config"
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
